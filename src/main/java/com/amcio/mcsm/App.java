45375bd3924ae5c9241d4adb1bdc6c4598cc8634
Amcio <amcio122@gmail.com>
2023-08-10 10:33:43 +0200

    Sync

---
+++ src/main/java/com/amcio/mcsm/App.java
@@ -0,0 +1,12 @@
+package com.amcio.mcsm;
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+
+import java.io.IOException;
+
+public class App {
+    public static void main( String[] args ) {
+
+    }
+}
---
+++ src/test/java/com/amcio/mcsm/AppTest.java
@@ -0,0 +1,37 @@
+package com.amcio.mcsm;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import com.amcio.mcsm.argparser.ArgParser;
+import com.amcio.mcsm.engine.MinecraftEngine;
+import org.junit.Test;
+
+/**
+ * Unit test for MinecraftServerManager.
+ */
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void argParserFindOption() {
+        ArgParser argParser = new ArgParser(new String[]{"-fabric"});
+        assertTrue(argParser.isOption("-fabric"));
+        assertFalse(argParser.isOption("-forge"));
+    }
+    @Test
+    public void regexVersionValidate() {
+        assertTrue(MinecraftEngine.validateVersion("1.20.1"));
+        assertTrue(MinecraftEngine.validateVersion("1.7.3"));
+        assertTrue(MinecraftEngine.validateVersion("1.7.10"));
+        assertTrue(MinecraftEngine.validateVersion("1.19"));
+        assertFalse(MinecraftEngine.validateVersion("1..20"));
+        assertFalse(MinecraftEngine.validateVersion("1.20a"));
+    }
+
+}
