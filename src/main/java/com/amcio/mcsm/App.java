7b8fc8b1349d3bbbc808f68dac2ffddeafb072af
Amcio <amcio122@gmail.com>
2023-08-17 15:34:30 +0200

    Begin argParser rework

--- src/main/java/com/amcio/mcsm/App.java
+++ src/main/java/com/amcio/mcsm/App.java
@@ -2,16 +2,35 @@ package com.amcio.mcsm;
 import com.amcio.mcsm.argparser.ArgParser;
 import com.amcio.mcsm.engine.Fabric;
 import com.amcio.mcsm.engine.Forge;
+import com.amcio.mcsm.engine.MinecraftEngine;
 import com.amcio.mcsm.engine.Paper;
 
 import java.io.IOException;
 
 public class App {
+
+    private static void printUsage(String[] args) {
+        System.out.println("Usage: " + args[0] + " -e <engine> -v <version> PATH");
+    }
     public static void main( String[] args ) throws IOException {
         ArgParser argParser = new ArgParser(args);
         String jarDestination = argParser.getPath(); // Either the path or "./", no need to worry about it later.
 
-        Forge forgeInstance = new Forge("1.20.1");
-        forgeInstance.download(jarDestination);
+        String selectedVersion = argParser.getValue("-v");
+        String selectedEngine = argParser.getValue("-e");
+
+        if (selectedVersion == null || selectedEngine == null) {
+            printUsage(args);
+            System.exit(1);
+        }
+
+        MinecraftEngine engine = switch (selectedEngine) {
+            case "forge" -> new Forge(selectedVersion);
+            case "fabric" -> new Fabric(selectedVersion);
+            case "vanilla" -> throw new UnsupportedOperationException("Downloading vanilla jar is not yet supported");
+            default -> new Paper(selectedVersion);
+        };
+
+        engine.download(jarDestination);
     }
 }
--- src/main/java/com/amcio/mcsm/argparser/ArgParser.java
+++ src/main/java/com/amcio/mcsm/argparser/ArgParser.java
@@ -1,6 +1,7 @@
 package com.amcio.mcsm.argparser;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class ArgParser {
     String[] args;
@@ -11,6 +12,22 @@ public class ArgParser {
     public boolean isOption(String option) {
         return Arrays.stream(args).anyMatch(str -> str.contains(option));
     }
+
+    public String getValue(String option) {
+        Iterator<String> argIterator = Arrays.stream(args).iterator();
+        while (argIterator.hasNext()) {
+            if (argIterator.next().equals(option) && argIterator.hasNext()) {
+                String value = argIterator.next();
+                if (value.startsWith("-")) {
+                    throw new IllegalArgumentException("No value was assigned to option: " + option);
+                }
+                return value;
+            }
+        }
+        return null;
+    }
+    // TODO: This function is now too simple, it will output one of the values instead of the path.
+    //  The path should be the last element in args
     public String getPath() {
         return Arrays.stream(args)
                 .filter(str -> !str.startsWith("-"))
