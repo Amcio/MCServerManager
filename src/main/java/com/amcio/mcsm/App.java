21b402c97b14edcae93128eb266eb63e79f5bb21
Amcio <amcio122@gmail.com>
2023-09-06 10:12:09 +0200

    Reflection tests

--- src/main/java/com/amcio/mcsm/App.java
+++ src/main/java/com/amcio/mcsm/App.java
@@ -15,27 +15,29 @@ public class App {
     public static void main(String[] args) throws IOException {
         ArgParser argParser = new ArgParser(new String[]{"test/"});
         String jarDestination = argParser.getPath(); // Either the path or "./", no need to worry about it later.
-        Method mainMethod = null;
 //        BaseMinecraftEngine Instance = new Vanilla("1.20.1", jarDestination);
 //        Instance.download();
         //TODO: Load the whole jar into the classpath
         URL url = new File(jarDestination, "forge-1.20.1-47.1.44-installer.jar").toURI().toURL();
-        String mainClass = null;
+        String mainClassName = null;
         try (URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{url})) {
             URL manifestURL = urlClassLoader.findResource("META-INF/MANIFEST.MF");
             Manifest manifest = new Manifest(manifestURL.openStream());
-            mainClass = manifest.getMainAttributes().getValue("Main-Class");
-            mainMethod = urlClassLoader.loadClass(mainClass).getMethod("main", String[].class);
+            mainClassName = manifest.getMainAttributes().getValue("Main-Class");
+            Class<?> mainClass = urlClassLoader.loadClass(mainClassName);
+            Method mainMethod = mainClass.getMethod("main", String[].class);
+            
+            try {
+                mainMethod.invoke(null, (Object) args);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
         } catch (ClassNotFoundException e) {
-            System.err.println("[!!!] Class: " + mainClass + " not found in JAR file.");
+            System.err.println("[!!!] Class: " + mainClassName + " not found in JAR file.");
             throw new RuntimeException(e);
         } catch (NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
-        try {
-            mainMethod.invoke(null, (Object) args);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 }
